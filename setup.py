5371d1ed22397593ac5f7c4af9563ae0b4fd477b
Andrés Rodríguez <andres.rodriguez@lithersoft.com>
2016-02-11 00:20:56 -0300

    Small update to setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@
 from distutils.core import setup
 
 setup(name='kivymd',
-	  version='0.1.2',
-	  description='Set of widgets for Kivy inspired by Google\'s Material '
-				  'Design',
-	  author='Andrés Rodríguez',
-	  author_email='andres.rodriguez@lithersoft.com',
-	  url='https://github.com/mixedCase/kivymd',
-	  packages=['kivymd'],
-	  package_data={'kivymd': ['images/*.png', 'images/*.jpg', 'images/*.atlas', 'fonts/*.ttf']},
-	  )
+      version='0.1.2',
+      description='Set of widgets for Kivy inspired by Google\'s Material '
+                  'Design',
+      author='Andrés Rodríguez',
+      author_email='andres.rodriguez@lithersoft.com',
+      url='https://github.com/mixedCase/kivymd',
+      packages=['kivymd'],
+      package_data={
+	      'kivymd': ['images/*.png', 'images/*.jpg', 'images/*.atlas',
+	                 'fonts/*.ttf']},
+      requires=['kivy']
+      )
