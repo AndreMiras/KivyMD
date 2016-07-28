9a879fb583f7f4190a4601a9a488ba61414395e0
frmdstryr <frmdstryr@gmail.com>
2016-07-28 16:20:57 -0400

    Add border as option (set via alpha)

--- kivymd/card.py
+++ kivymd/card.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from kivy.lang import Builder
-from kivy.properties import BoundedNumericProperty, ReferenceListProperty
+from kivy.properties import BoundedNumericProperty, ReferenceListProperty, ListProperty,BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.elevationbehavior import ElevationBehavior
 from kivymd.theming import ThemableBehavior
@@ -15,6 +15,11 @@ Builder.load_string('''
             size: self.size
             pos: self.pos
             radius: [self.border_radius]
+        Color:
+            rgba: self.theme_cls.divider_color
+            a: self.border_color_a
+        Line:
+            rounded_rectangle: (self.pos[0],self.pos[1],self.size[0],self.size[1],self.border_radius) 
     background_color: self.theme_cls.bg_light
 ''')
 
@@ -26,4 +31,5 @@ class MDCard(ThemableBehavior, ElevationBehavior, BoxLayout):
     a = BoundedNumericProperty(0., min=0., max=1.)
     
     border_radius = BoundedNumericProperty(dp(3),min=0)
+    border_color_a = BoundedNumericProperty(0, min=0., max=1.)
     background_color = ReferenceListProperty(r, g, b, a)
