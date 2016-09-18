4694321731e6efd289e4a62157bd455e28867e3b
ZingBallyhoo <zingballyhoo@live.co.uk>
2016-09-18 18:55:50 +0100

    Fix package_data folder names

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='kivymd',
       packages=['kivymd'],
       package_data={
           'kivymd': ['images/*.png', 'images/*.jpg', 'images/*.atlas',
-                     'fonts/*.ttf', 'vendor/circularLayout/*.py',
-                     'vendor/circleTimePicker/*.py']},
+                     'fonts/*.ttf', 'vendor/circleLayout/*.py',
+                     'vendor/circularTimePicker/*.py']},
       requires=['kivy']
       )
