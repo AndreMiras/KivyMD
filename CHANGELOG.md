9b2206adbfc8138dd67ec114df19bef96e6bd894
Andre Miras <andre.miras@gmail.com>
2018-09-17 23:53:16 +0200

    Fix BaseButton to allow buttons to be disabled
    the super() is unneeded since no classes which BaseButton inherits from
    have an on_disabled() function. This super(), therefore, causes a crash.
    Credits:
    https://gitlab.com/kivymd/KivyMD/merge_requests/52

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+
+
+## [Unreleased]
+
+  - Fixes `BaseButton.on_disabled` `AttributeError`
+
+
+## [20170825]
+
+  - Initial fork from git@gitlab.com:kivymd/KivyMD.git @19e587e6
--- kivymd/button.py
+++ kivymd/button.py
@@ -175,7 +175,6 @@ class BaseButton(ThemableBehavior, ButtonBehavior,
             self._current_button_color = self.md_bg_color_disabled
         else:
             self._current_button_color = self.md_bg_color
-        super(BaseButton, self).on_disabled(instance, value)
 
 
 class BasePressedButton(BaseButton):
