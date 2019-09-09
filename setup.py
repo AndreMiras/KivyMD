39b2fc0327beb4f75941ae17efea43264903dc8b
Andre Miras <andre.miras@gmail.com>
2019-09-10 00:15:50 +0200

    Setup cleanup and fixes
    Still a lot of technical debt left to fix.

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-
+import os
 import re
-from distutils.core import setup
+from setuptools import setup
 
 VERSION_FILE = "kivymd/__init__.py"
 ver_file_data = open(VERSION_FILE, "rt").read()
@@ -12,19 +11,41 @@ if ver_reg_search:
 else:
     raise ValueError("Unable to find version string in {}.".format(VERSION_FILE))
 
-setup(name='kivymd',
-      version=version,
-      description='Set of widgets for Kivy inspired by Google\'s Material '
-                  'Design',
-      author='Andrés Rodríguez',
-      author_email='andres.rodriguez@lithersoft.com',
-      url='https://github.com/AndreMiras/KivyMD',
-      packages=['kivymd'],
-      package_data={
-          'kivymd': ['images/*.png', 'images/*.jpg', 'images/*.atlas',
-                     'vendor/*.py',
-                     'fonts/*.ttf', 'vendor/circleLayout/*.py',
-                     'vendor/circularTimePicker/*.py',
-                     'vendor/navigationdrawer/*.py']},
-      requires=['kivy']
-      )
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup_params = {
+    'name': 'kivymd',
+    'version': version,
+    'description': "Set of widgets for Kivy inspired by Google's Material Design",
+    'long_description': read('README.md'),
+    'long_description_content_type': 'text/markdown',
+    'author': 'Andrés Rodríguez',
+    'author_email': 'andres.rodriguez@lithersoft.com',
+    'url': 'https://github.com/AndreMiras/KivyMD',
+    'packages': ['kivymd'],
+    'package_data': {
+        'kivymd': [
+            'images/*.png',
+            'images/*.jpg',
+            'images/*.atlas',
+            'vendor/*.py',
+            'fonts/*.ttf', 'vendor/circleLayout/*.py',
+            'vendor/circularTimePicker/*.py',
+            'vendor/navigationdrawer/*.py',
+        ]
+    },
+    'requires': ['kivy'],
+}
+
+
+def run_setup():
+    setup(**setup_params)
+
+
+# makes sure the setup doesn't run at import time
+if __name__ == '__main__':
+    run_setup()
